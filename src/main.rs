9426e4b88f3f6a1225f95918df404f6f33dac7d1
TyanNN <tyannn@cocaine.ninja>
2016-11-12 12:23:27 +0300

    HTML больше ненужен, заменён на специальный макро

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ use iron::mime::{Mime, TopLevel, SubLevel, Attr, Value};
 use staticfile::Static;
 use mount::Mount;
 use urlencoded::UrlEncodedQuery;
+use router::Router;
 
 use std::path::Path;
 use std::fs::{File,OpenOptions};
@@ -102,10 +103,70 @@ fn more(req: &mut Request) -> IronResult<Response> {
     Ok(response)
 }
 
+fn show(req: &mut Request) -> IronResult<Response> {
+    let id = req.extensions.get::<Router>().and_then(|x| x.find("id")).and_then(|x| x.parse::<i32>().ok()).unwrap();
+    let image = match DB.lock().unwrap().get_image(id).unwrap() {
+        Some(x) => x,
+        None    => return Ok(Response::with(status::NotFound))
+    };
+
+    let page = html!{
+        meta charset="utf-8" /
+        link rel="stylesheet" href="/assets/css/milligram.min.css" /
+        link rel="stylesheet" href="/assets/css/main.css" /
+        link rel="icon" type="image/jpeg" href="/assets/favicon.jpg" /
+        script src="/assets/js/show.js" {}
+        title { "Zeph - " (image.tags.join(" ")) }
+        meta property="og:title" content="Zeph" /
+        meta property="og:description" content=(image.tags.join(" ")) /
+        meta property="og:url" content={ "https://zeph.kotobank.ch/show/" (image.id) } /
+        meta property="og:image" content={"https://zeph.kotobank.ch/images/preview/" (image.name)} /
+
+        div style="width:100%;" {
+            div.tags-search {
+                a href="/" title="Boop!" {
+                    img#nano-logo src="/assets/logo.jpg" /
+                    h3 style="display: inline; vertical-align: 50%" "Zeph"
+                }
+                form#tag-search-form action="/search" {
+                    input#tag-search-field placeholder="Search" name="q" type="text" /
+                }
+                div#id { "#" (image.id) }
+                div#tags {
+                    div#image-info {
+                        @if let Some(original_link) = image.original_link {
+                            a#original-link href=(original_link) "Original page" br /
+                        }
+                        @if let Some(rating) = image.rating {
+                            a#rating href={ "/search?q=rating:" (rating) } { "rating:" (rating) } br /
+                        }
+                        @if let Some(got_from) = image.got_from {
+                            a#got_from href={ "/search?q=from:" (got_from) } { "from:" (got_from) } br /
+                        }
+                        @if let Some(uploader) = image.uploader {
+                            a#uploader href={ "/search?q=uploader:" (uploader) } { "uploader:" (uploader) } br /
+                        }
+                        div#vote-area div#score { "Score: " (image.score) } br /
+                        @for tag in image.tags {
+                            a href={ "/search?q=" (tag) } { (tag) } br /
+                        }
+                    }
+                }
+            }
+            a href={ "/images/" (image.name) } {
+                img#image-block style="display: block; margin: 0 auto;" src={ "/images/" (image.name) } /
+            }
+        }
+    };
+
+    Ok(Response::with((status::Ok, page)))
+}
+
 fn main() {
     let router = router!(index: get "/" => index_n_search,
                          more: get "/more" => more,
-                         search: get "/search" => index_n_search);
+                         search: get "/search" => index_n_search,
+                         show: get "/show/:id" => show);
 
     let mut mount = Mount::new();
     mount.mount("/", router)

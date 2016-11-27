88aa3ec4bfeddb0da1eea3614fb12f95d074ca4f
TyanNN <tyannn@cocaine.ninja>
2016-11-27 17:11:53 +0300

    Probably should escape symbols

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -69,6 +69,7 @@ pub use self::postgres::Db;
 
 /// Tag parsing
 fn parse_tag(tag: &str) -> Tag {
+    let tag = tag.replace('\'', "''").replace(r'\\', r"\\");
     let all = tag.split(':').collect::<Vec<_>>();
     match all.len() {
         1 => {
